7c2813eb094ebf389eede4c550fb86d04fe8260d
IamSanketMore <sanketmore.999.m@gmail.com>
2020-12-12 17:24:43 +0530

    Add EmpwageComputation to UC5

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,1 +1,24 @@
-echo " UC-4 :- Solving using Switch Case Statement "
+#!/bin/bash -x
+
+empCheck=$((RANDOM%3))
+isFullTime=1
+isPartTime=2
+salary=0
+ratePerHr=20
+
+case $empCheck in
+        $isfullTime)
+        echo "FullTime Employee"
+        empHrs=8
+        ;;
+        $isPartTime)
+        echo "PartTime Employee"
+        empHrs=4
+        ;;
+        *)
+        echo "Employee is Absent"
+        empHrs=0
+        ;;
+esac
+salary=$(($ratePerHr*$empHrs))
+echo "Employee Wage:-" $salary
