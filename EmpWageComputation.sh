72e6bf1b319e979bd4fa93065b4ed56bb4ef611b
IamSanketMore <sanketmore.999.m@gmail.com>
2020-12-12 17:35:26 +0530

    Add EmpwageComputation to UC6

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,24 +1,32 @@
-#!/bin/bash -x
+#!/bin/bash
 
-empCheck=$((RANDOM%3))
 isFullTime=1
 isPartTime=2
 salary=0
 ratePerHr=20
+numberOfWorkingDays=20;
 
-case $empCheck in
-        $isfullTime)
-        echo "FullTime Employee"
-        empHrs=8
-        ;;
-        $isPartTime)
-        echo "PartTime Employee"
-        empHrs=4
-        ;;
-        *)
-        echo "Employee is Absent"
-        empHrs=0
-        ;;
-esac
-salary=$(($ratePerHr*$empHrs))
-echo "Employee Wage:-" $salary
+for ((day=1;day<=$numberOfWorkingDays;day++))
+do
+        empCheck=$((RANDOM%3))
+        case $empCheck in
+                $isfullTime)
+                echo "FullTime Employee"
+                empHrs=8
+                ;;
+                $isPartTime)
+                echo "PartTime Employee"
+                empHrs=4
+                ;;
+                *)
+                echo "Employee is Absent"
+                empHrs=0
+                ;;
+        esac
+
+        salary=$(( $ratePerHr*$empHrs ))
+        echo "Per day salary" $salary
+        totalSalary=$(($totalSalary+$salary))
+done
+echo "Employee Wage per month:-" $totalSalary
+
